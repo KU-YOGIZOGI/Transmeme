e5e2e524d314b1561209d16f5969ac0833af3815
ahhyun1015 <121007805+ahhyun1015@users.noreply.github.com>
2023-11-08 01:34:28 +0900

    dic 수정, asset 이름 변경

---
+++ Transmeme/TestViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TestViewController.swift
+//  Transmeme
+//
+//  Created by 정아현 on 11/8/23.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class TestViewController: UIViewController {
+    
+}
