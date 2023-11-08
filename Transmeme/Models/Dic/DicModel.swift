1cb388982cfbe57a91bd976d4cac97d2077ab51d
hsseor <hsseor@naver.com>
2023-11-08 19:18:08 +0900

    [Chore] 폴더링

---
+++ Transmeme/Models/Dic/DicModel.swift
@@ -0,0 +1,9 @@
+//
+//  DicModel.swift
+//  Transmeme
+//
+//  Created by 홍서린 on 2023/11/08.
+//
+
+import Foundation
+//
---
+++ Transmeme/Models/Trans/TransModel.swift
@@ -0,0 +1,9 @@
+//
+//  TransModel.swift
+//  Transmeme
+//
+//  Created by 홍서린 on 2023/11/08.
+//
+
+import Foundation
+//
