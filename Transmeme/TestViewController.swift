f4d6c381d4afe8dc8692b8c83f6c036a4c727fda
ahhyun1015 <121007805+ahhyun1015@users.noreply.github.com>
2023-11-08 17:47:06 +0900

    test 최종-1 수정함

--- Transmeme/TestViewController.swift
+++ Transmeme/TestViewController.swift
@@ -130,14 +130,29 @@ class TestViewController: UIViewController {
         $0.attributedText = attrString
     }
     let nextButton = UIButton().then {
+        $0.setTitle("다음", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 18)
+        $0.setTitleColor(UIColor.white, for: .normal)
         $0.setImage(UIImage(named: "nextButton"), for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFit
+        $0.titleLabel?.lineBreakMode = .byWordWrapping
+        
+        let spacing: CGFloat = 7.0
+        $0.contentEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
+        $0.titleEdgeInsets = UIEdgeInsets(top: 0, left: -($0.imageView?.frame.width ?? 0) - spacing, bottom: 0, right: $0.imageView?.frame.width ?? 0)
+        $0.imageEdgeInsets = UIEdgeInsets(top: 0, left: $0.titleLabel?.frame.width ?? 0, bottom: 0, right: -($0.titleLabel?.frame.width ?? 0) - spacing)
+        
+        $0.semanticContentAttribute = .forceRightToLeft
     }
-    let nextLabel = UILabel().then {
-        $0.text = "다음"
-        $0.textColor = UIColor.white
-        $0.font = UIFont(name: "GmarketSansMedium", size: 18)
-        $0.numberOfLines = 0
+    let correctImageView = UIImageView().then {
+        $0.image = UIImage(named: "correct")
+        $0.alpha = 0
     }
+    let incorrectImageView = UIImageView().then {
+        $0.image = UIImage(named: "incorrect")
+        $0.alpha = 0
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -166,8 +181,10 @@ class TestViewController: UIViewController {
         self.view.addSubview(hintLabel)
         self.view.addSubview(hintLabel2)
         self.view.addSubview(nextButton)
-        self.view.addSubview(nextLabel)
         applyConstraintsToBottomSection()
+        
+        quizButton.addTarget(self, action: #selector(didTapquizButton), for: .touchUpInside)
+        quizButton2.addTarget(self, action: #selector(didTapquizButton2), for: .touchUpInside)
     }
     
     func applyConstraintsToTopSection() {
@@ -257,7 +274,6 @@ class TestViewController: UIViewController {
     }
     
     func applyConstraintsToBottomSection() {
-        let safeArea = view.safeAreaLayoutGuide
         hintView.snp.makeConstraints { make in
             make.width.equalTo(quizButton)
             make.height.equalTo(91)
@@ -279,23 +295,30 @@ class TestViewController: UIViewController {
             make.centerX.equalTo(hintView)
         }
         nextButton.snp.makeConstraints { make in
-            make.width.equalTo(11)
-            make.height.equalTo(20)
+            make.width.equalTo(52)
+            make.height.equalTo(22)
             make.trailing.equalTo(quizView.snp.trailing).offset(-34)
-            make.top.equalTo(hintView.snp.bottom).offset(24)
-        }
-        nextLabel.snp.makeConstraints { make in
-            make.top.equalTo(hintView.snp.bottom).offset(24)
             make.leading.equalTo(quizView.snp.leading).offset(264)
+            make.top.equalTo(hintView.snp.bottom).offset(24)
+        }
+    }
+    
+    @objc func didTapquizButton(_ sender: Any) {
+        let addVC = TestIncorrectViewController()
+        addVC.modalPresentationStyle = .overFullScreen
+        present(addVC, animated: false) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                addVC.dismiss(animated: false, completion: nil)
+            }
+        }
+    }
+    @objc func didTapquizButton2(_ sender: Any) {
+        let addVC = TestCorrectViewController()
+        addVC.modalPresentationStyle = .overFullScreen
+        present(addVC, animated: false) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                addVC.dismiss(animated: false, completion: nil)
+            }
         }
     }
 }
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
-
-struct ViewControllerPreview: PreviewProvider {
-    static var previews: some View {
-        TestViewController().showPreview(.iPhone14Pro)
-    }
-}
-#endif
