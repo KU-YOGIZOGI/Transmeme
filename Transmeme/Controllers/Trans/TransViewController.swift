6a31dab46889390f8b92e8626b636d2c3a02cc76
hsseor <hsseor@naver.com>
2023-11-14 23:37:09 +0900

    [Chore] meanVIew 제약조건 수정

--- Transmeme/Controllers/Trans/TransViewController.swift
+++ Transmeme/Controllers/Trans/TransViewController.swift
@@ -255,7 +255,7 @@ class TransViewController: UIViewController {
         
         barImage.snp.makeConstraints{
             $0.top.equalTo(subTitleLabel.snp.bottom).offset(20)
-            $0.leading.equalTo(44)
+            $0.leading.equalTo(36)
             $0.width.equalTo(2)
             $0.height.equalTo(15)
         }
@@ -361,7 +361,7 @@ class TransViewController: UIViewController {
         exampleSection.snp.makeConstraints {
             $0.top.equalTo(meaningSection.snp.bottom).offset(5)
             $0.leading.trailing.equalTo(meaningSection)
-            $0.height.equalTo(40)
+            $0.height.equalTo(45)
             
         }
         
